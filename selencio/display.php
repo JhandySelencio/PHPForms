9c4d4294b038d5c2f0856d9824b990323bbd834e
JhandySelencio <154280174+JhandySelencio@users.noreply.github.com>
2025-08-26 16:18:27 +0800

    Add files via upload

--- selencio/display.php
+++ selencio/display.php
@@ -1,9 +1,25 @@
-<html>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
 <body>
-
-Welcome <?php echo $_POST["fname"]; ?><br>
-Your family name is: <?php echo $_POST["lname"]; ?><br>
-Your Id number is: <?php echo $_POST["idNum"]; ?><br>
-Your Department is: <?php echo $_POST["dept"]; ?>
+<div class="displayCon">
+    <form action="index.php">
+        <div>
+          Welcome <b><?php echo $_POST["fname"]; ?></b>,<br>
+          Your family name is <b><?php echo $_POST["lname"]; ?></b><br>
+          Your ID Number is <b><?php echo $_POST["idNum"]; ?></b><br>
+          Your Course is <b><?php echo $_POST["course"]; ?></b>
+        </div>
+        <br>
+        <div>
+             <input type="Submit" value="Return">
+        </div>  
+    </form>
+</div>
 </body>
 </html>
--- selencio/index.php
+++ selencio/index.php
@@ -7,27 +7,38 @@
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
-
-    <?php echo "Hello World";?>
 <!-- http://localhost/selencio/home-page.html -->
-    <div>
-        <h2>Student Registration Forms</h2>
-
-        <form action="display.php" method="POST">
-          <label for="fname">First name:</label><br>
-          <input type="text" id="fname" name="fname" value="Kiervy">
-          <br>
-          <label for="lname">Last name:</label><br>
-          <input type="text" id="lname" name="lname" value="Hernani">
-          <br>
-          <label for="idnum">Id number:</label><br>
-          <input type="text" id="idNum" name="idNum">
-          <br>
-          <label for="dept">Department:</label><br>
-          <input type="text" id="dept" name="dept">
-          <br><br>
-          <input type="submit" value="Submit">
-        </form> 
+    <div class="mainCon">
+        <div>
+            <h1>Student Registration Form</h1>
+        </div>
+        <div>
+            <form action="display.php" method="POST">
+                <div>
+                    <label for="fname">First name:</label><br>
+                    <input type="text" id="fname" name="fname">
+                </div>
+                <br>
+                <div>
+                    <label for="lname">Last name:</label><br>
+                    <input type="text" id="lname" name="lname">
+                </div>
+                <br>
+                <div>
+                    <label for="idnum">ID number:</label><br>
+                    <input type="text" id="idNum" name="idNum">
+                </div>
+                <br>
+                <div>
+                    <label for="course">Course:</label><br>
+                    <input type="text" id="course" name="course">
+                </div>
+                <br><br>
+                <div>
+                    <input type="submit" value="Submit">
+                </div>
+              </form> 
+        </div>
     </div>
 </body>
 </html>
